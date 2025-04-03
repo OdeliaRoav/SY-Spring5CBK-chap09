57b433267b342b7a103da430ec39dc7200734225
SYU <xeungjin@gmail.com>
2025-04-03 11:30:50 +0900

    initial commit

---
+++ src/main/java/sp5/chap09_jsp/Chap09JspApplication.java
@@ -0,0 +1,13 @@
+package sp5.chap09_jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap09JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap09JspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sp5/chap09_jsp/Chap09JspApplicationTests.java
@@ -0,0 +1,13 @@
+package sp5.chap09_jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap09JspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
