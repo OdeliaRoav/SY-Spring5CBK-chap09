583f7d65f00f5e4edc5a0dc38f2893947aa47d52
SYU <xeungjin@gmail.com>
2025-04-03 16:37:32 +0900

    ~ 290 page

---
+++ src/main/java/sp5/chap09_jsp/controller/MainController.java
@@ -0,0 +1,13 @@
+package sp5.chap09_jsp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/main")
+    public String main() {
+        return "main";
+    }
+}
