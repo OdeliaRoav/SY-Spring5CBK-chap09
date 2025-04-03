e5f533aa48daa9a51733acce7f3aaa66ebc60b3c
SYU <xeungjin@gmail.com>
2025-04-03 16:20:01 +0900

    ~ 289 page

--- src/main/java/sp5/chap09_jsp/controller/RegisterController.java
+++ src/main/java/sp5/chap09_jsp/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package sp5.chap09_jsp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,10 +30,11 @@ public class RegisterController {
     }
 
     @PostMapping("/register/step2")
-    public String handleStep2(@RequestParam(value = "agree", defaultValue = "false") Boolean agree) {
+    public String handleStep2(@RequestParam(value = "agree", defaultValue = "false") Boolean agree, Model model) {
         if (!agree) {
             return "register/step1";
         }
+        model.addAttribute("registerRequest", new RegisterRequest());
         return "register/step2";
     }
 
